ecb0dfa7a3d0f19e4789925d9e3a756bbfdb0deb
YuutaOokoshi <ookoshiyuuta@gmail.com>
2019-01-05 11:58:47 +0000

    config.ini作成, push_harvestのリファクタリング

--- push_harvest.py
+++ push_harvest.py
@@ -6,15 +6,36 @@ import urllib.request
 
 import os
 import time
+import configparser
+ 
+# debug用
+from pprint import pprint
+ 
+ini = configparser.ConfigParser()
+
+# iniファイル存在チェック
+INI_PATH = os.path.dirname(__file__)+'/config.ini'
+if not os.path.exists(INI_PATH):
+    print('ERR : "./config.ini" is not found.')
+    sys.exit(1)
+
+ini.read(INI_PATH, 'UTF-8')
+
 # import logging
-
 # logging.config.fileConfig("logging_debug.conf")
-
 # logger = logging.getLogger()
 
 # soracom Inventoryキー
-DEVICE_ID = 'デバイスキー'
-SERCRET_KEY = 'セキュリティーキー'
+## config.iniを参照
+DEVICE_ID = ini.get('soracom', 'device_id')
+SERCRET_KEY = ini.get('soracom', 'sercret_key')
+
+if DEVICE_ID == '' or SERCRET_KEY == '':
+    print('ERR : "device key" or "sercret key" is not found. ')
+    sys.exit(1)
+print('soracom inventory keys is ')
+print('    '+ini.get('soracom', 'device_id'))
+print('    '+ini.get('soracom', 'sercret_key'))
 
 # GPS諸々設定
 gps_socket = gps3.GPSDSocket()
@@ -29,82 +50,49 @@ for new_data in gps_socket:
 
     # logger.debug('get for gps data')
     if new_data:
+        # logger.info(filename +'is not exists')
+        data_stream.unpack(new_data)
+        
+        # Bluttoothボタンの大小どちらか押したかによって生死が変わる
         if os.path.exists(filename):
-            # logger.info(filename +'is exists')
-            data_stream.unpack(new_data)
-            print('is_alive : 1')
-            print('time : ', data_stream.TPV['time'])
-            print('lat : ', data_stream.TPV['lat'])
-            print('lon : ', data_stream.TPV['lon'])
-            print('alt : ', data_stream.TPV['alt'])
-            print('speed : ', data_stream.TPV['speed'])
-            
-            if data_stream.TPV['time'] == 'n/a' \
-                or data_stream.TPV['lat'] == 'n/a' \
-                or data_stream.TPV['lon'] == 'n/a' \
-                or data_stream.TPV['alt'] == 'n/a' \
-                or data_stream.TPV['speed'] == 'n/a' :
-                # ごみなので捨てる
-                # logger.info('gps data is null')
-                
-                continue
-            
-            url = 'https://api.soracom.io/v1/devices/'+DEVICE_ID+'/publish'
-            data = {
-                'is_alive' : 1,
-                'time' : data_stream.TPV['time'],
-                'lat' : data_stream.TPV['lat'],
-                'lon' : data_stream.TPV['lon'],
-                'alt' : data_stream.TPV['alt'],
-                'speed' : data_stream.TPV['speed'],
-            }
-            headers = {
-                'x-device-secret': SERCRET_KEY,
-            }
-
-            req = urllib.request.Request(url, json.dumps(data).encode(), headers)
-            with urllib.request.urlopen(req) as res:
-                body = res.read()
-                time.sleep(1)
-            # logger.info('gps data is pushed')
-            # logger.debug(req)
-
+            data_stream.TPV['is_alive'] = 1
         else:
-            # logger.info(filename +'is not exists')
-            data_stream.unpack(new_data)
-            print('is_alive : 0')
-            print('time : ', data_stream.TPV['time'])
-            print('lat : ', data_stream.TPV['lat'])
-            print('lon : ', data_stream.TPV['lon'])
-            print('alt : ', data_stream.TPV['alt'])
-            print('speed : ', data_stream.TPV['speed'])
-            
-            if data_stream.TPV['time'] == 'n/a' \
-                or data_stream.TPV['lat'] == 'n/a' \
-                or data_stream.TPV['lon'] == 'n/a' \
-                or data_stream.TPV['alt'] == 'n/a' \
-                or data_stream.TPV['speed'] == 'n/a' :
-                # ごみなので捨てる
-                # logger.info('gps data is null')
-                
-                continue  
-            
-            url = 'https://api.soracom.io/v1/devices/'+DEVICE_ID+'/publish'
-            data = {
-                'is_alive' : 0,
-                'time' : 'n/a', # data_stream.TPV['time'], 
-                'lat' : 'n/a', # data_stream.TPV['lat'], 
-                'lon' : 'n/a', # data_stream.TPV['lon'], 
-                'alt' : 'n/a', # data_stream.TPV['alt'], 
-                'speed' : 'n/a', # data_stream.TPV['speed'], 
-            }
-            headers = {
-                'x-device-secret': SERCRET_KEY,
-            }
+            data_stream.TPV['is_alive'] = 0
 
-            req = urllib.request.Request(url, json.dumps(data).encode(), headers)
-            with urllib.request.urlopen(req) as res:
-                body = res.read()
-                time.sleep(1)
-            # logger.info('gps data is pushed')
-            # logger.debug(req)
+        print("~~~~~~~~~~~")
+        print('is_alive : ', data_stream.TPV['is_alive'])
+        print('time : ', data_stream.TPV['time'])
+        print('lat : ', data_stream.TPV['lat'])
+        print('lon : ', data_stream.TPV['lon'])
+        print('alt : ', data_stream.TPV['alt'])
+        print('speed : ', data_stream.TPV['speed'])
+
+        # if data_stream.TPV['time'] == 'n/a' \
+        #     or data_stream.TPV['lat'] == 'n/a' \
+        #     or data_stream.TPV['lon'] == 'n/a' \
+        #     or data_stream.TPV['alt'] == 'n/a' \
+        #     or data_stream.TPV['speed'] == 'n/a' :
+        #     # ごみなので捨てる
+        #     # logger.info('gps data is null')    
+        #     continue
+  
+        url = 'https://api.soracom.io/v1/devices/'+DEVICE_ID+'/publish'
+        data = {
+            'is_alive' : data_stream.TPV['is_alive'],
+            'time' : data_stream.TPV['time'],
+            'lat' : data_stream.TPV['lat'],
+            'lon' : data_stream.TPV['lon'],
+            'alt' : data_stream.TPV['alt'],
+            'speed' : data_stream.TPV['speed'],
+        }
+        headers = {
+            'x-device-secret': SERCRET_KEY,
+        }
+        pprint(json.dumps(data).encode())
+
+        req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+        with urllib.request.urlopen(req) as res:
+            body = res.read()
+            time.sleep(1)
+        # logger.info('gps data is pushed')
+        # logger.debug(req)
